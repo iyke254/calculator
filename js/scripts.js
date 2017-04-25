8dee23ed5b5156c891ab0f54ff24b5c92a759f67
Isaac Onyango <nderiony95@gmail.com>
2017-04-25 14:18:31 +0300

    add the html file and folders

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var subtract = function(number1, number2) {
+return number1 - number2;
+};
+
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter a number"));
+
+
+alert(subtract(number1, number2));
+
